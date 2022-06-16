606fef1ecea1473b00f04d88348942940dabf430
Arif Su'admaji <104288379+Arifsuadmaji@users.noreply.github.com>
2022-06-16 08:52:08 +0700

    Add files via upload

---
+++ Pertemuan 2/Latihan 3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+P = 5*5
+L = 30/3
+Luas = P*L
+print('Panjang ruangan :',P,"m")
+print('Lebar ruangan :',L,"m")
+print('Luas ruangan dalah :',Luas,"m2")
+
