7e990b97d339b506221b6144d02f74846156ec9a
Arif Su'admaji <104288379+Arifsuadmaji@users.noreply.github.com>
2022-06-16 08:54:32 +0700

    Add files via upload

---
+++ Pertemuan 2/Latihan 3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+P = 5*5
+L = 30/3
+Luas = P*L
+print('Panjang ruangan :',P,"m")
+print('Lebar ruangan :',L,"m")
+print('Luas ruangan dalah :',Luas,"m2")
+
+
+# In[2]:
+
+
+P = float(input('Masukan panjang :'))
+L = float(input('Masukan Lebar :'))
+
+Luas = P*L
+print('Panjang ruangan :',P,"m")
+print('Lebar ruangan :',L,"m")
+print('Luas ruangan dalah :',Luas,"m2")
+
