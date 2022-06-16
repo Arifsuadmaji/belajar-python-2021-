750f252dc5bbf1f3438bab241118a457a59f1c3b
Arif Su'admaji <104288379+Arifsuadmaji@users.noreply.github.com>
2022-06-16 08:46:31 +0700

    Add files via upload

---
+++ Pertemuan 1/Latihan 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Nama = 'Wanti'
+Umur = 94/(46/23)
+Agama = 'Islam'
+Gender = 'Wanita'
+Alamat = 'Tanjung kurung'
+Keterangan = "Motivasi"
+
+print('Nama :', Nama,'\nUmur :', Umur,'Tahun','\nAgama :', Agama,'\nGender :', Gender,'\nAlamat :', Alamat)
+print('Merupakan ibu saya yang menjadi',Gender,'terbaik yang pertama kali saya kenal dan sebagai guru yang selalu menjadi',Keterangan,'untuk saya')
+
+
+# In[ ]:
+
+
+
+
